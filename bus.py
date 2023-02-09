79ded005a4af11ac9fce1a341ccf7e1158885c5e
kameranis <kameranis@gmail.com>
2023-02-09 16:45:15 -0600

    Complete the API calls as functions and add helper functions that pull multiple data.

--- bus.py
+++ bus.py
@@ -1,23 +1,35 @@
-import requests
 import logging
-from typing import Union, List
-
-import pandas as pd
+from typing import Union, List, Dict
+import requests
 
 from cta_secrets import BUS_API_KEY
 
 BASE_URL = 'http://www.ctabustracker.com/bustime/api/v2/'
 VEHICLES_ENDPOINT = 'getvehicles'
 ROUTES_ENDPOINT = 'getroutes'
+DIRECTIONS_ENDPOINT = 'getdirections'
+STOPS_ENDPOINT = 'getstops'
+PATTERNS_ENDPOINT = 'getpatterns'
+PREDICTIONS_ENDPOINT = 'getpredictions'
 
 logging.basicConfig(filename='logs/bus.log', format='%(asctime)s\t%(levelname)s\t%(message)s', level=logging.WARN)
+
+MAX_ROUTES_PER_CALL = 10
+MAX_PATTERNS_PER_CALL = 10
+MAX_STOPS_PER_CALL = 10
+MAX_VEHICLES_PER_CALL = 10
 
 
 class APIError(Exception):
     pass
 
 
-def call_api(route: str, **params) -> dict:
+def call_api(route: str, **params) -> Dict:
+    """Base function to perform requests to the CTA bus API and handle errors
+
+    :param route:
+
+    """
     try:
         response = requests.get(BASE_URL + route, params={
             'key': BUS_API_KEY,
@@ -37,7 +49,7 @@ def call_api(route: str, **params) -> dict:
     return dict()
 
 
-def get_vehicles(routes: Union[str, List[str]], tmres: str = 's') -> list:
+def get_vehicles(routes: Union[str, List[str]], tmres: str = 's') -> List:
     route_param = routes if isinstance(routes, str) else ','.join(routes)
     if tmres not in ['m', 's']:
         raise ValueError('Parameter `tmres` can only be one of [\'m\', \'s\']')
@@ -45,6 +57,104 @@ def get_vehicles(routes: Union[str, List[str]], tmres: str = 's') -> list:
     return js.get('vehicle', list())
 
 
-def get_routes() -> list:
+def get_routes() -> List:
     js = call_api(ROUTES_ENDPOINT)
     return js.get('routes', list())
+
+
+def get_all_vehicles() -> List:
+    routes = get_routes()
+    rts = [rt['rt'] for rt in routes]
+    vehicles = []
+    for i in range(0, len(rts), MAX_ROUTES_PER_CALL):
+        vehicles.extend(get_vehicles(routes=rts[i:i + MAX_ROUTES_PER_CALL]))
+    return vehicles
+
+
+def get_directions(route: str) -> List:
+    js = call_api(DIRECTIONS_ENDPOINT, rt=route)
+    return js.get('directions', list())
+
+
+def get_all_directions() -> Dict[str, List]:
+    routes = get_routes()
+    rts = [rt['rt'] for rt in routes]
+    directions = {rt: [direction['dir']
+                       for direction in get_directions(rt)
+                       if 'dir' in direction]
+                  for rt in rts}
+    return directions
+
+
+def get_route_stops(route: str, direction: str) -> List:
+    js = call_api(STOPS_ENDPOINT, rt=route, dir=direction)
+    return js.get('stops', list())
+
+
+def get_all_stops(directions: Union[None, Dict[str, List[str]]] = None) -> Dict[str, Dict[str, List]]:
+    if directions is None:
+        directions = get_all_directions()
+    all_stops = {rt: {rt_direction: get_route_stops(rt, rt_direction)
+                      for rt_direction in rt_directions}
+                 for rt, rt_directions in directions.items()}
+    return all_stops
+
+
+def get_patterns_from_pids(pids: List[Union[str, int]]) -> Dict[int, Dict]:
+    patterns = {}
+    for i in range(0, len(pids), MAX_PATTERNS_PER_CALL):
+        js = call_api(PATTERNS_ENDPOINT, pid=','.join(str(pid) for pid in pids[i:i+MAX_PATTERNS_PER_CALL]))
+        for ptr in js['ptr']:
+            patterns[ptr['pid']] = ptr
+    return patterns
+
+
+def get_pattern_from_rt(rt: str) -> Dict[int, Dict]:
+    js = call_api(PATTERNS_ENDPOINT, rt=rt)
+    patterns = {ptr['pid']: ptr for ptr in js['ptr']}
+    return patterns
+
+
+def get_all_patterns() -> Dict[int, Dict]:
+    routes = get_routes()
+    rts = [rt['rt'] for rt in routes]
+    patterns = {}
+    for rt in rts:
+        rt_patterns = get_pattern_from_rt(rt)
+        for pid, _ in rt_patterns.items():
+            rt_patterns[pid]['rt'] = rt
+        patterns.update(rt_patterns)
+    return patterns
+
+
+def get_predictions_from_stops(stops: Union[str, List[str]], rts: Union[None, str, List[str]] = None) -> List[Dict]:
+    predictions = []
+    if isinstance(stops, str):
+        stops = [stops]
+    if isinstance(rts, list):
+        rts = ','.join(rts)
+    for i in range(0, len(stops), MAX_STOPS_PER_CALL):
+        if rts is None:
+            predictions.extend(call_api(PREDICTIONS_ENDPOINT, stpid=','.join(stops[i:i + MAX_STOPS_PER_CALL]))['prd'])
+        else:
+            predictions.extend(call_api(PREDICTIONS_ENDPOINT, stpid=','.join(stops[i:i+ MAX_STOPS_PER_CALL]), rt=rts)['prd'])
+    return predictions
+
+
+def get_predictions_from_vehicles(vehicles: Union[str, List[str]]) -> List[Dict]:
+    if isinstance(vehicles, str) and ',' in vehicles:
+        vehicles = vehicles.split(',')
+    if isinstance(vehicles, str):
+        return call_api(PREDICTIONS_ENDPOINT, vid=vehicles)['prd']
+    predictions = []
+    for i in range(0, len(vehicles), MAX_VEHICLES_PER_CALL):
+        js = call_api(PREDICTIONS_ENDPOINT, vid=','.join(vehicles[i:i + MAX_VEHICLES_PER_CALL]))
+        predictions.extend(js.get('prd', []))
+    return predictions
+
+
+def get_all_predictions() -> List[Dict]:
+    vehicles = get_all_vehicles()
+    vids = [vehicle['vid'] for vehicle in vehicles]
+    predictions = get_predictions_from_vehicles(vids)
+    return predictions
--- track_buses.py
+++ track_buses.py
@@ -9,21 +9,13 @@ import bus
 CALL_INTERVAL = 150
 BUS_ROUTES_FILE = 'cta_data/bus_numbers.txt'
 OUTPUT_FILE = 'bus_tracking.csv'
-MAX_ROUTES_PER_CALL = 10
-
-
-def track_buses(out_f: TextIO) -> None:
-    routes = bus.get_routes()
-    rts = [rt['rt'] for rt in routes]
-    for i in range(0, len(rts), MAX_ROUTES_PER_CALL):
-        vehicles = bus.get_vehicles(routes=rts[i:i + MAX_ROUTES_PER_CALL])
-        df = pd.DataFrame(vehicles)
-        df.to_csv(out_f, header=False, index=False)
 
 
 def repeated_track_buses(scheduler: sched.scheduler, out_f: TextIO, call_interval: float = CALL_INTERVAL) -> None:
     scheduler.enter(call_interval, 1, repeated_track_buses, (scheduler, out_f, call_interval))
-    track_buses(out_f=out_f)
+    vehicles = bus.get_all_vehicles()
+    df = pd.DataFrame(vehicles)
+    df.to_csv(out_f, header=False, index=False)
 
 
 def main():
