8cc18220220e1b34a85e62f7ed61fb679c02fb86
kameranis <kameranis@gmail.com>
2023-02-23 23:42:17 -0600

    Change shapefile to have string datetime.

--- build_shapefile.py
+++ build_shapefile.py
@@ -1,21 +1,26 @@
+import sys
 from datetime import datetime
 import pandas
 import shapefile
 
-from track_buses import OUTPUT_FILE as BUS_DATA
+from track_buses import BUS_OUTPUT_FILE as BUS_DATA
 
 # the Writer generates multiple files with different extensions
 OUTPUT_PREFIX = "bus_tracking"
 
-df = pandas.read_csv(BUS_DATA, names=['vid', 'tmstmp', 'lat', 'lon', 'hdg', 'pid', 'rt', 'des', 'pdist',
-                                      'dly', 'tatripid', 'origtatripno', 'tablockid', 'zone'])
 
-with shapefile.Writer(OUTPUT_PREFIX) as shp:
-    shp.field('vehicle_id', 'N')
-    shp.field('timestamp', 'N')  # the 'D' type can only store dates, no times, so we'll use a Unix timestamp here
-    shp.field('route', 'C', size=4)
-    shp.field('destination', 'C')
+if __name__ == '__main__':
+    df = pandas.read_csv(sys.argv[1], names=['vid', 'tmstmp', 'lat', 'lon', 'hdg', 'pid', 'rt', 'des', 'pdist',
+                                          'dly', 'tatripid', 'origtatripno', 'tablockid', 'zone'])
 
-    for row in df.itertuples(index=False):
-        shp.point(row.lon, row.lat)
-        shp.record(row.vid, int(datetime.strptime(row.tmstmp, "%Y%m%d %H:%M:%S").timestamp()), row.rt, row.des)
+    with shapefile.Writer(sys.argv[2]) as shp:
+        shp.field('vehicle_id', 'N')
+        shp.field('timestamp', 'C')  # the 'D' type can only store dates, no times, so we'll use a Unix timestamp here
+        shp.field('route', 'C', size=4)
+        shp.field('destination', 'C')
+
+        for row in df.itertuples(index=False):
+            shp.point(row.lon, row.lat)
+            shp.record(row.vid, row.tmstmp, row.rt, row.des)
+
+
